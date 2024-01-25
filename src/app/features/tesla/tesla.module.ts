f468e2d66bbeee7a0f3500d5f47cee3fd9daed79
r.fabian <r.fabian@resource-it.ro>
2024-01-25 11:54:36 +0200

    create tesla module and add to routing

---
+++ src/app/features/tesla/tesla.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TeslaRoutingModule } from './tesla-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TeslaRoutingModule
+  ]
+})
+export class TeslaModule { }
