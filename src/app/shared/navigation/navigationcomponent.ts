bfea59a1b37c3da2b38bf0ec4ef9e74df8a51a14
r.fabian <r.fabian@resource-it.ro>
2024-01-26 10:05:04 +0200

    add roouter parameter

--- src/app/shared/navigation/navigationcomponent.ts
+++ src/app/shared/navigation/navigationcomponent.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { ConfiguredTeslaService } from '../../features/tesla/services/configured-tesla.service';
+import { ConfiguredTesla } from '../../features/tesla/services/models';
+import { ImageService } from '../tesla-image-viewer/image.service';
 
 @Component({
   selector: 'app-navigation',
@@ -8,9 +12,25 @@ import { Router } from '@angular/router';
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss'
 })
-export class NavigationComponent {
+export class NavigationComponent implements OnInit, OnDestroy {
+  private configuredTesla: ConfiguredTesla | null = null;
+  private subSink: Subscription = new Subscription();
 
-  constructor(private router: Router) {
+  constructor(
+    private configuredTeslaService: ConfiguredTeslaService,
+    private imageService: ImageService,
+    private router: Router) {
+  }
+
+  ngOnInit() {
+    const configuredTesla$ = this.configuredTeslaService.configuration$;
+
+    const self = this;
+    const subscription = configuredTesla$.subscribe((configuredTesla) => {
+      self.configuredTesla = configuredTesla;
+    });
+
+    this.subSink.add(subscription);
   }
 
   public openTeslaModelConfig(): void {
@@ -18,10 +38,26 @@ export class NavigationComponent {
   }
 
   public openModelOptionsConfig(): void {
-    this.router.navigate(['/config/options']);
+    let url = '/config/options/:modelCode';
+
+    if (this.configuredTesla === null || this.configuredTesla.modelCode === null) {
+      return;
+    }
+
+    url = url.replace(':modelCode', this.configuredTesla.modelCode)
+
+    this.router.navigate([url]);
   }
 
   public openConfigSummaryConfig(): void {
     this.router.navigate(['/config/summary']);
+  }
+
+  ngOnDestroy(): void {
+    if (this.subSink.closed) {
+      return;
+    }
+
+    this.subSink.unsubscribe();
   }
 }
