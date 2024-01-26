5261fe73af76e370789bebd24aa46bc8bf3b4009
r.fabian <r.fabian@resource-it.ro>
2024-01-26 09:52:49 +0200

    add implementation to component

--- src/app/shared/tesla-image-viewer/tesla-image-viewer.component.ts
+++ src/app/shared/tesla-image-viewer/tesla-image-viewer.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ConfiguredTesla } from '../../features/tesla/services/models';
 import { ImageService } from './image.service';
 import { ConfiguredTeslaService } from '../../features/tesla/services/configured-tesla.service';
@@ -11,7 +11,7 @@ import { Subscription } from 'rxjs';
   templateUrl: './tesla-image-viewer.component.html',
   styleUrl: './tesla-image-viewer.component.scss'
 })
-export class TeslaImageViewerComponent {
+export class TeslaImageViewerComponent implements OnInit, OnDestroy {
 
   private configuredTesla: ConfiguredTesla | null = null;
   private subSink: Subscription = new Subscription();
